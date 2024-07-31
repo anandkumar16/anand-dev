6c3b326bfdb59ca320d520830f36362eb9317161
anandkumar16 <anandyadav16022004@gmail.com>
2024-08-01 01:35:10 +0530

    added portfolios

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Button } from "@/components/ui/moving-border";
 const About = () => {
   return (
-    <div className='bg-black h-full w-full text-white sm:flex sm:justify-around about py-12 mt-8 overflow-x-hidden' id='about'>
-      <div className='flex flex-col justify-around'>
-        <div className='sm:px-16 px-2'>
-          <h2 className='text-4xl sm:text-5xl font-extrabold mt-16'>Introduction</h2>
-          <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>
+    <div className="bg-black h-full w-full text-white sm:flex sm:justify-around about py-12 mt-8 overflow-x-hidden' id='about">
+      <div className="flex flex-col justify-around">
+        <div className="sm:px-16 px-2">
+          <h2 className="text-4xl sm:text-5xl font-extrabold mt-16">Introduction</h2>
+          <p className="mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]">
             Hi! I'm Anand, a BMSIT student and passionate full stack developer skilled in NextJS, TypeScript, TailwindCSS, and the MERN stack (MongoDB, Express.js, React.js, Node.js).
           </p>
-          <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>
+          <p className="mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]">
             I love tackling coding challenges, collaborating with developers, and creating efficient solutions. With a solid foundation in front-end and back-end technologies and over 150 LeetCode problems solved, I deliver high-quality, scalable code.
           </p>
-          <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>
+          <p className="mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]">
             Tools & Technologies:
             <br/>
             Frontend: React, HTML5, CSS3, JavaScript, Tailwind CSS, NextJS
@@ -24,14 +24,14 @@ const About = () => {
             Version Control: Git, GitHub
           </p>
         </div>  
-        <div className='flex justify-start m-14'>
+        <div className="flex justify-start m-14">
         <Button
         borderRadius="1.75rem"
         className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
       >
         Resume
       </Button>
-          <a href='https://leetcode.com/anand016/' target='_blank'>
+          <a href="https://leetcode.com/anand016/" target="_blank">
           <Button
           
         borderRadius="1.75rem"
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 const Contact = () => {
@@ -8,16 +8,16 @@ const Contact = () => {
 
 	return (
 
-        <div className='relative z-0 bg-black w-screen h-screen mt-12'>   
-			<div className='text-white contact overflow-x-hidden pt-12 mt-8 ' id='contact'>
-				<div className='z-10 w-full sm:w-[650px] m-auto p-8 rounded-2xl' >
+        <div className="relative z-0 bg-black w-screen h-screen mt-12">   
+			<div className="text-white contact overflow-x-hidden pt-12 mt-8 ' id='contact">
+				<div className="z-10 w-full sm:w-[650px] m-auto p-8 rounded-2xl" >
 					<p className='font-light'>REACH OUT TO ME</p>
 					<h2 className='text-5xl font-extrabold mt-2 bg-clip-text text-transparent bg-gradient-to-r from-gray-500 to-pink-500'>Contact.</h2>
 					<form
 
-						className='mt-12 flex flex-col gap-8'
+						className="mt-12 flex flex-col gap-8"
 					>
-						<label className='flex flex-col'>
+						<label className="flex flex-col">
 							<span className=' font-medium mb-4'>Your Name</span>
 							<input
 								type='text'
@@ -39,15 +39,15 @@ const Contact = () => {
 								required
 							/>
 						</label>
-						<label className='flex flex-col'>
-							<span className='font-medium mb-4'>Your Message</span>
+						<label className="flex flex-col">
+							<span className="font-medium mb-4">Your Message</span>
 							<textarea
 								rows={4}
-								name='message'
+								name="message"
 								value={""}
 								
-								placeholder='Do you have anything to say?'
-								className='py-4 px-6 rounded-lg outline-none border-none font-medium bg-gray-900'
+								placeholder="Do you have anything to say?"
+								className="py-4 px-6 rounded-lg outline-none border-none font-medium bg-gray-900"
 								required
 							/>
 						</label>
