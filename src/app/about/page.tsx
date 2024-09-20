f82a7ab38767468b17e3c61cc8c73717789103f6
anandkumar16 <anandyadav16022004@gmail.com>
2024-09-20 11:24:38 +0530

    added projects

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import ButtonLink from "@/components/ButtonLink";
+import Footer from "@/components/Footer";
 const About = () => {
   return (
+    <div>
     <div className="bg-black h-full w-full text-white sm:flex sm:justify-around about py-12 mt-8 overflow-x-hidden" id="about">
       <div className="flex flex-col justify-around">
         <div className="sm:px-16 px-2">
           <h2 className="text-4xl sm:text-5xl font-extrabold mt-16">Introduction</h2>
           <p className="mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]">
-            Hi! I'm Anand, a BMSIT student and passionate full stack developer skilled in NextJS, TypeScript, TailwindCSS, and the MERN stack (MongoDB, Express.js, React.js, Node.js).
+            Hi! I'm Anand, 3rd year student and passionate full stack developer skilled in NextJS, TypeScript, TailwindCSS, and the MERN stack (MongoDB, Express.js, React.js, Node.js).
           </p>
           <p className="mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]">
-            I love tackling coding challenges, collaborating with developers, and creating efficient solutions. With a solid foundation in front-end and back-end technologies and over 150 LeetCode problems solved, I deliver high-quality, scalable code.
+            I love tackling coding challenges, collaborating with developers, and creating efficient solutions. With a solid foundation in front-end and back-end technologies and over 200 LeetCode problems solved, I deliver high-quality, scalable code.
           </p>
           <p className="mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]">
             Tools & Technologies:
             <br/>
             Languages : C, C++, Javacript, TypeScript.
             <br/>
-            Framework : ExpressJS, NodeJS, ReactJS, NextJs, Hono.
+            Framework : ExpressJS, NodeJS, ReactJS, NextJS, Hono.
             <br/>
             Libraries : Bootstrap, TailwindCSS.
             <br/>
@@ -34,6 +36,8 @@ const About = () => {
           />
         </div>
       </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,54 +1,64 @@
 'use client'
 import React from "react";
-import Image from "next/image";
 import { GlareCard } from "@/components/ui/glare-card";
+import Link from "next/link";
+import Footer from "@/components/Footer";
 
 const courses = [
   {
-    title: "Chess.com",
-    description: "This is an online chess game built using Express, Socket.io, and the Chess.js library.",
-    image: "/chess.png" 
-  },
-  {
-    title: "Medium Clone",
-    description: "Medium-like blogging platform built using TypeScript, PostgreSQL, Express, React, and Wrangler",
-    image: "image.png" 
+    title: "Homify",
+    description: "Homify is a vacation rental platform that enables users to browse, book, and manage properties.",
+    image: "/airbnb.jpg",
+    link: "https://homify-2.onrender.com/"
   },
   {
     title: "AI SketchBook",
     description: "A NextJs project using Canva.js , Tailwind CSS,with drawing, erasing, redo, undo, and download features. ",
-    image: "sketch.png" 
+    image: "sketchpad.jpg",
+    link: "https://sketchpad-pi.vercel.app/"
+  },
+  {
+    title: "Chess.com",
+    description: "This is an online chess game built using Express, Socket.io, and the Chess.js library.",
+    image: "/chess.png",
+    link: "https://chess-com-xkwc.onrender.com/"
+  },
+  {
+    title: "Medium Clone",
+    description: "Medium-like blogging platform built using TypeScript, PostgreSQL, Express, React, and Wrangler",
+    image: "image.png",
+    link: "www.google.com" 
   },
   {
     title: "Trackify",
     description: "An EJS, Socket.io, and Express application used to track any person's location in real-time.",
-    image: "loc.png" 
+    image: "loc.png",
+    link: "https://trackify-d3ha.onrender.com/"
   },
   {
     title: "Music academy",
     description: "A Next.js application with Aceternity UI, designed for selling music courses online.",
-    image: "music.png" 
+    image: "music.png",
+    link: "https://music-academy1.onrender.com/"
   },
   {
     title: "AI text-generator",
     description: " this is an application which uses expressJS and utilized the Gemini AI model to generate text.",
-    image: "text.png" 
+    image: "text.png",
+    link: "www.google.com"
   },
   {
     title: "Telegram Bot",
     description: "An automated bot for Telegram, designed to interact with users and perform specific tasks.",
-    image: "bot.png" 
+    image: "bot.png",
+    link: "www.google.com"
   },
   {
     title: "payTM clone",
     description: "Developed a digital payment application using React, Express, Tailwind CSS, and PostgreSQL.",
-    image: "paytm.png" 
-  },
-  {
-    title: "AI-Saas",
-    description: " A Next.js and ShadCN application capable of generating text, images, code, and videos from user input.",
-    image: "saas.png" 
-  },
+    image: "paytm.png",
+    link: "www.google.com"
+  }
 ];
 
 function page() {
@@ -57,20 +67,21 @@ function page() {
     <div className="min-h-screen bg-black mt-40">
         <div className="pt-10 px-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4 gap-8" >
         {courses.map((course, index) => (
-          <GlareCard key={index} className="flex flex-col items-start justify-end py-8 px-6">
-          <img
-          className="h-60 w-full absolute inset-0 object-cover"
-          src={course.image} alt="image"
-        />
+          <Link key={index} href={course.link} target="_blank" rel="noopener noreferrer">
+          <GlareCard className="flex flex-col items-start justify-end py-8 px-6">
+            <img
+              className="h-60 w-full absolute inset-0 object-cover"
+              src={course.image} alt="image"
+            />
             <p className="font-bold text-white text-lg">{course.title}</p>
             <p className="font-normal text-base text-neutral-200 mt-4">
               {course.description}
             </p>
-          {/* <button>GitHub</button>
-          <button>Live</button> */}
           </GlareCard>
+        </Link>
         ))}
     </div>
+    <Footer/>
     </div>
   )
 }
